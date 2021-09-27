9472caba9457e7d4f17077bc068da732b721f080
EdwinLopez27 <88947351+EdwinLopez27@users.noreply.github.com>
2021-09-26 21:25:10 -0600

    P2
    s

---
+++ 201700763_PARCIAL2SUPERMERCADO/201700763_SUPERMERCADO/201700763_SUPERMERCADO/Form1.Designer.vb
@@ -0,0 +1,409 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MENÚToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.PROCESARToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LIMPIARENTRADASToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LIMPIARVECTORESToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SALIRToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(290, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(267, 32)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "SUPERMERCADO"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MENÚToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1173, 28)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MENÚToolStripMenuItem
+        '
+        Me.MENÚToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PROCESARToolStripMenuItem, Me.LIMPIARENTRADASToolStripMenuItem, Me.LIMPIARVECTORESToolStripMenuItem, Me.SALIRToolStripMenuItem})
+        Me.MENÚToolStripMenuItem.Name = "MENÚToolStripMenuItem"
+        Me.MENÚToolStripMenuItem.Size = New System.Drawing.Size(65, 24)
+        Me.MENÚToolStripMenuItem.Text = "MENU"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(26, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 24)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "NIT"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(26, 153)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(99, 24)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "NOMBRE"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(26, 210)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(121, 24)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "DIRECCION"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(204, 104)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(227, 22)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(204, 155)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(227, 22)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(204, 210)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(227, 22)
+        Me.TextBox3.TabIndex = 7
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(890, 124)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(229, 132)
+        Me.ListBox1.TabIndex = 8
+        '
+        'PROCESARToolStripMenuItem
+        '
+        Me.PROCESARToolStripMenuItem.Name = "PROCESARToolStripMenuItem"
+        Me.PROCESARToolStripMenuItem.Size = New System.Drawing.Size(226, 26)
+        Me.PROCESARToolStripMenuItem.Text = "PROCESAR"
+        '
+        'LIMPIARENTRADASToolStripMenuItem
+        '
+        Me.LIMPIARENTRADASToolStripMenuItem.Name = "LIMPIARENTRADASToolStripMenuItem"
+        Me.LIMPIARENTRADASToolStripMenuItem.Size = New System.Drawing.Size(226, 26)
+        Me.LIMPIARENTRADASToolStripMenuItem.Text = "LIMPIAR ENTRADAS"
+        '
+        'LIMPIARVECTORESToolStripMenuItem
+        '
+        Me.LIMPIARVECTORESToolStripMenuItem.Name = "LIMPIARVECTORESToolStripMenuItem"
+        Me.LIMPIARVECTORESToolStripMenuItem.Size = New System.Drawing.Size(226, 26)
+        Me.LIMPIARVECTORESToolStripMenuItem.Text = "LIMPIAR VECTORES"
+        '
+        'SALIRToolStripMenuItem
+        '
+        Me.SALIRToolStripMenuItem.Name = "SALIRToolStripMenuItem"
+        Me.SALIRToolStripMenuItem.Size = New System.Drawing.Size(226, 26)
+        Me.SALIRToolStripMenuItem.Text = "SALIR"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(26, 296)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(264, 24)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "NOMBRE DEL PRODUCTO"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(26, 352)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(267, 24)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "CANTIDAD DE PRODUCTO"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(26, 405)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(251, 24)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "PRECIO DEL PRODUCTO"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(379, 296)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(227, 22)
+        Me.TextBox4.TabIndex = 12
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(379, 418)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(227, 22)
+        Me.TextBox5.TabIndex = 13
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 16
+        Me.ListBox2.Location = New System.Drawing.Point(890, 371)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(209, 228)
+        Me.ListBox2.TabIndex = 15
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(886, 72)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(182, 24)
+        Me.Label8.TabIndex = 16
+        Me.Label8.Text = "DATOS FACTURA"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(886, 317)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(213, 24)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "DATOS PRODUCTOS"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(26, 618)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(77, 24)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "TOTAL"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(277, 618)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(227, 22)
+        Me.TextBox6.TabIndex = 19
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(23, 488)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(182, 24)
+        Me.Label11.TabIndex = 20
+        Me.Label11.Text = "FORMA DE PAGO"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(277, 488)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(111, 22)
+        Me.CheckBox1.TabIndex = 22
+        Me.CheckBox1.Text = "EFECTIVO"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox2.Location = New System.Drawing.Point(413, 486)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(232, 24)
+        Me.CheckBox2.TabIndex = 23
+        Me.CheckBox2.Text = "TARJETA DE CREDITO"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(379, 371)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(227, 22)
+        Me.TextBox7.TabIndex = 24
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(26, 555)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(137, 24)
+        Me.Label12.TabIndex = 25
+        Me.Label12.Text = "DESCUENTO"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(277, 543)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(227, 22)
+        Me.TextBox8.TabIndex = 26
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(543, 541)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(89, 24)
+        Me.Label13.TabIndex = 27
+        Me.Label13.Text = "Recargo"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(638, 543)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(181, 22)
+        Me.TextBox9.TabIndex = 28
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1173, 682)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MENÚToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PROCESARToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LIMPIARENTRADASToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LIMPIARVECTORESToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SALIRToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents TextBox9 As TextBox
+End Class
---
+++ 201700763_PARCIAL2SUPERMERCADO/201700763_SUPERMERCADO/201700763_SUPERMERCADO/Form1.vb
@@ -0,0 +1,62 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub LIMPIARENTRADASToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LIMPIARENTRADASToolStripMenuItem.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+        TextBox4.Text = ""
+        TextBox5.Text = ""
+        TextBox6.Text = ""
+
+
+    End Sub
+
+    Private Sub LIMPIARVECTORESToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LIMPIARVECTORESToolStripMenuItem.Click
+        TextBox7.Text = ""
+        TextBox8.Text = ""
+        TextBox9.Text = ""
+        ListBox1.Items.Clear()
+        ListBox2.Items.Clear()
+
+    End Sub
+
+    Private Sub PROCESARToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PROCESARToolStripMenuItem.Click
+        ListBox1.Items.Add(TextBox1.Text)
+        ListBox1.Items.Add(TextBox2.Text)
+        ListBox1.Items.Add(TextBox3.Text)
+        ListBox2.Items.Add(TextBox4.Text)
+        ListBox2.Items.Add(TextBox5.Text)
+        ListBox2.Items.Add(TextBox6.Text)
+        ListBox2.Items.Add(TextBox7.Text)
+        ListBox2.Items.Add(TextBox8.Text)
+
+        If TextBox7.Text >= 5 Then
+            TextBox8.Text = Val(TextBox5.Text * TextBox7.Text * 0.05)
+            TextBox6.Text = Val(TextBox5.Text * TextBox7.Text - TextBox8.Text)
+        End If
+
+        If TextBox7.Text < 5 Then
+            TextBox8.Text = Val(0)
+            TextBox6.Text = Val(TextBox5.Text * TextBox7.Text)
+        End If
+
+        If CheckBox2.Checked Then
+            TextBox9.Text = Val(TextBox5.Text * TextBox7.Text * 0.08)
+            TextBox6.Text = Val(TextBox5.Text * TextBox7.Text) + Val(TextBox8.Text)
+        End If
+
+    End Sub
+
+    Private Sub SALIRToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SALIRToolStripMenuItem.Click
+        If MsgBox("¿Desea salir del programa?", vbQuestion + vbYesNo, "Pregunta") = vbYes Then
+            End
+        End If
+    End Sub
+
+    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+
+    End Sub
+End Class
